dec0eda50a085caa161f210f4a13df747705b6d8
Nafiu Amosa <naf.amosa@gmail.com>
2025-10-23 14:15:02 +0100

    initial commit

---
+++ Location-Aware-App/Location-Aware-App/Location_Aware_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Location_Aware_AppApp.swift
+//  Location-Aware-App
+//
+//  Created by Nafiu Amosa on 23/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Location_Aware_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
